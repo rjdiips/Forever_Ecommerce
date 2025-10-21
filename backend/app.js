630b618ac86f33ab64449c32d64b66854d872abc
Rajdeep Sen <rjdiips#gmail.com>
2025-10-21 21:46:57 +0530

    fix typo in import statement

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/mongodb.js';
-import connectToCloudinary from './config/cloudinay.js';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
-import cartRouter from './routes/cartRouter.js';
-import orderRouter from './routes/orderRouter.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectToCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import cartRouter from "./routes/cartRouter.js";
+import orderRouter from "./routes/orderRouter.js";
 
 // App Config
 const app = express();
@@ -19,13 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 // API Endpoints
-app.use('/api/user', userRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 // Listener
